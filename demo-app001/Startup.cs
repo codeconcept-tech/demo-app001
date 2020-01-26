3af2be1e1c36eb65706f41a125fb27f560e7e366
saqib <gaming.guru24x7@gmail.com>
2020-01-27 00:35:14 +0500

    another modification to startup.cs

--- demo-app001/Startup.cs
+++ demo-app001/Startup.cs
@@ -48,7 +48,7 @@ namespace demo_app001
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             // app.UseStaticFiles();
             app.UseCookiePolicy();
 
